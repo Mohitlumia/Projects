3f8170fabb08929fa39b62e350e0f3e003cc168b
Mohit Gupta <mohitproduct@gmail.com>
2024-02-06 21:15:26 +0530

    quantifing scratch area

--- Scratch_Analysis/Scratch_Analysis.py
+++ Scratch_Analysis/Scratch_Analysis.py
@@ -5,6 +5,7 @@ from matplotlib import pyplot as plt
 from skimage.morphology import disk
 from skimage.filters.rank import entropy
 from skimage.filters import threshold_otsu
+import numpy as np
 #%%
 path = "scratch_images/Scratch0.jpg"
 img = io.imread(path)
@@ -14,5 +15,9 @@ entropy_img = entropy(img,disk(10))
 # get threshold value and create a binary segmented image
 threshold_val = threshold_otsu(entropy_img)
 segmented_img = entropy_img > threshold_val
+#%%
+# quantify the scratch area and print
+scratch_area_percentage = (np.sum(segmented_img == False)/segmented_img.size)*100
+print(scratch_area_percentage)
 #%%
 plt.imshow(segmented_img,cmap="gray")

31a21fcd4c0835cf7a63c10f58239b6c552f00bc
Mohit Gupta <mohitproduct@gmail.com>
2024-02-06 20:45:48 +0530

    entropy applied

--- Scratch_Analysis/Scratch_Analysis.py
+++ Scratch_Analysis/Scratch_Analysis.py
@@ -2,10 +2,12 @@
 #%%
 from skimage import io
 from matplotlib import pyplot as plt
-
+from skimage.morphology import disk
+from skimage.filters.rank import entropy
 #%%
 path = "scratch_images/Scratch0.jpg"
 img = io.imread(path)
-
+# applying entropy to segment based to texture
+entropy_img = entropy(img,disk(10))
 #%%
-plt.imshow(img,cmap="gray")
+plt.imshow(entropy_img,cmap="gray")

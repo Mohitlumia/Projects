8bbe8a713639ca06cfa47b6b7304d1bdafecdcec
Mohit Gupta <mohitproduct@gmail.com>
2024-02-06 20:51:36 +0530

    binary segmented

--- Scratch_Analysis/Scratch_Analysis.py
+++ Scratch_Analysis/Scratch_Analysis.py
@@ -4,10 +4,15 @@ from skimage import io
 from matplotlib import pyplot as plt
 from skimage.morphology import disk
 from skimage.filters.rank import entropy
+from skimage.filters import threshold_otsu
 #%%
 path = "scratch_images/Scratch0.jpg"
 img = io.imread(path)
 # applying entropy to segment based to texture
 entropy_img = entropy(img,disk(10))
 #%%
-plt.imshow(entropy_img,cmap="gray")
+# get threshold value and create a binary segmented image
+threshold_val = threshold_otsu(entropy_img)
+segmented_img = entropy_img > threshold_val
+#%%
+plt.imshow(segmented_img,cmap="gray")
